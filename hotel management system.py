a770d6ad9674382f723763a9e037067ede602dd2
sachin rawat <sachinrawat6264384464@gmail.com>
2025-02-21 23:22:23 +0530

    Update hotel management system.py

--- hotel management system.py
+++ hotel management system.py
@@ -1,9 +1,10 @@
 # cafe management system using database mysql and python  module mysql.connector
 import mysql.connector as mycon
-mydb=mycon.connect(host="localhost", user="root", password="",database="cafe_management")
+mydb=mycon.connect(host="localhost", user="root", password="",database="cafe_management") # create a connect with database mysql
 db_cursor=mydb.cursor()
+#all query we use inthis project
 """
-query="create database cafe_management"   # create database
+query="create database cafe_management"   # create database        
 query2="create table menu(item_name varchar(30), price int(10))"
 query3="create table fast_food(item_name varchar(30), price int(10))"
 query5="create table chapati(item_name varchar(30), price int(10))"
@@ -20,6 +21,7 @@ count=0
 total=0
 order_food=[]
 while True:
+#this is a starting point when we enter a hotel
     print("----welcome to foody cafe -----")
     print("1. call better for order food type ---> better")
     print("1. call better for bill type ---> bill")
@@ -29,6 +31,7 @@ while True:
     if person=="better":
       
         while True:
+            #this is a menu list with different items 
             print()
             print("-----weldth of food---------")
             print("1.😋 light food enter---> 1")
@@ -36,9 +39,9 @@ while True:
             print("3.😋 chapati category enter---> 3")
             print("4.🍵🍻🍹🍺 drink catagary enter---> 4")
             print("5.💸💸 for exit and time of payment --> 5")
-            food_choice=int(input("write your choice here = "))
+            food_choice=int(input("write your choice here = "))# this is use for choice a items that you want to eat
             
-            if food_choice==1:
+            if food_choice==1:# choice 1 you are enter into light food items  and follow the command for order 
             
                 while True:
                     print()
@@ -47,8 +50,8 @@ while True:
                     print("3. for conform order enter --> 3")
                     choice=int(input("enter your choice here = "))
                     
-                    if choice==1:
-                        query="select item_name,price from fast_food"
+                    if choice==1:   # choice 1 for display all items list withh price
+                        query="select item_name,price from fast_food"  # query use for fetch items form database mysql
                         db_cursor.execute(query)
                         result=db_cursor.fetchall()
                         if result:
@@ -59,56 +62,67 @@ while True:
                             print("--------items ends---------")
                         else:
                             print("no item found")
+                            
+                    # if you choice 2 now you can order the items        
                     elif choice==2:
-                        quentity =int(input("enter how much items you want to oredr ="))
+                        quentity =int(input("enter how much items you want to order ="))  #this is for quentity of order
                         for i in range(1,quentity+1):
                             food_name=input("write the name of your orders = ")
                             order_food.append(food_name)
                             
-                            query="select price from fast_food where item_name= %s"
+                            query="select price from fast_food where item_name= %s"  #query for fetch data from database
                             db_cursor.execute(query,(food_name,))
                             result=db_cursor.fetchone()
                             for i in result:
                               total+=i
                               
-                    elif choice==3:
+                    elif choice==3: # this choice for conformed order
                        print("✅✅ your order conform successfully! injoy food!")
                        break
+                       
+                    else:# no item found show this
+                       print("you try to choice invalide choice!🔴")
             
-            elif food_choice==2:
+            elif food_choice==2:# choice 2 you are enter into heavy food items  and follow the command for order 
                 while True:
                     print()
                     print("1.display all item enter--> 1")
                     print("2. for order enter --> 2")
                     print("3. for conform order enter --> 3")
                     choice=int(input("enter your choice here = "))
-                    
+                    # choice 1 for display all items list withh price
                     if choice==1:
-                        query="select item_name,price from menu"
+                        query="select item_name,price from menu"# query use for fetch items form database mysql
                         db_cursor.execute(query)
                         result=db_cursor.fetchall()
-                        if result:
+                        try:
                             print("\n😋fast_food order now")
                             
                             for result in enumerate(result):
                                print(result ) 
                             print("--------items ends---------")
-                        else:
+                        except FileExistsError:
                             print("no item found") 
+                    # if you choice 2 now you can order the items 
                     elif choice==2:
-                        quentity =int(input("enter how much items you want to oredr ="))
+                        quentity =int(input("enter how much items you want to order ="))#this is for quentity of orde
                         for i in range(1,quentity+1):
                             food_name=input("write the name of your orders = ")
                             order_food.append(food_name)
                             
-                            query="select price from menu where item_name= %s"
+                            query="select price from menu where item_name= %s" #query for fetch data from database
                             db_cursor.execute(query,(food_name,))
                             result=db_cursor.fetchone()
                             for i in result:
                               total+=i
-                    elif choice==3:
+                              
+                    elif choice==3:# this choice for conformed order
                        print("✅✅ your order conform successfully! injoy food!")
                        break
+                       
+                    else:# no item found show this
+                       print("you try to choice invalide choice!🔴")
+            # choice 3 you are enter into chapati category items  and follow the command for order
             elif food_choice==3:
                 while True:
                     print()
@@ -116,9 +130,9 @@ while True:
                     print("2. for order enter --> 2")
                     print("3. for conform order enter --> 3")
                     choice=int(input("enter your choice here = "))
-                    
+                    # choice 1 for display all items list withh price
                     if choice==1:
-                        query="select item_name,price from chapati"
+                        query="select item_name,price from chapati"# query use for fetch items form database mysql
                         db_cursor.execute(query)
                         result=db_cursor.fetchall()
                         if result:
@@ -129,22 +143,25 @@ while True:
                             print("--------items ends---------")
                         else:
                             print("no item found")
-                    
+                    # if you choice 2 now you can order the items
                     elif choice==2:
                         food_name=input("write the name of your orders = ")
                         order_food.append(food_name)
-                        query="select price from chapati where item_name= %s"
+                        query="select price from chapati where item_name= %s"#query for fetch data from database
                         db_cursor.execute(query,(food_name,))
                         result=db_cursor.fetchone()
                         price=int(result[0])
                         quentity =int(input("enter quentity of chapati ="))
                         for _ in range(quentity):
                             total+=price
-                        
+                    # this choice for conformed order
                     elif choice==3:
                        print("✅✅ your order conform successfully! injoy food!")
-                       break    
-            
+                       break  
+                       
+                    else:# no item found show this
+                       print("you try to choice invalide choice!🔴")
+            # choice 4 you are enter into drink catagary enter items  and follow the command for order
             elif food_choice==4:
                 while True:
                     print()
@@ -152,9 +169,9 @@ while True:
                     print("2. for order enter --> 2")
                     print("3. for conform order enter --> 3")
                     choice=int(input("enter your choice here = "))
-                    
+                    # choice 1 for display all items list withh price
                     if choice==1:
-                        query="select item_name,price from drink_items"
+                        query="select item_name,price from drink_items"# query use for fetch items form database mysql
                         db_cursor.execute(query)
                         result=db_cursor.fetchall()
                         if result:
@@ -165,20 +182,27 @@ while True:
                             print("--------items ends---------")
                         else:
                             print("no item found") 
+                    # if you choice 2 now you can order the items
                     elif choice==2:
                         quentity =int(input("enter how much items you want to oredr ="))
                         for i in range(1,quentity+1):
                             food_name=input("write the name of your orders = ")
                             order_food.append(food_name)
                             
-                            query="select price from drink_items where item_name= %s"
+                            query="select price from drink_items where item_name= %s"#query for fetch data from database
                             db_cursor.execute(query,(food_name,))
                             result=db_cursor.fetchone()
                             for i in result:
                               total+=i
+                    # this choice for conformed order
                     elif choice==3:
                        print("✅✅ your order conform successfully! injoy food!")
                        break
+                       
+                    else:# no item found show this
+                       print("you try to choice invalide choice!🔴")
+                       
+            #this command for exite form the loop while and time for bill
             elif food_choice==5:
                 break
             
@@ -186,7 +210,7 @@ while True:
             
             else:
                print("enter valide food choice! try again")
-               
+     #this is for the time of bill count all the items you order and with total payment          
     elif person=="bill":
         print("💸💸Oreder food and total billings")
         index=1
